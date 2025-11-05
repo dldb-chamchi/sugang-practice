5b32c5165009405cd5b69cf672dca4658554c2d8
dldb-chamchi <dladkfl49@gmail.com>
2025-11-05 11:35:55 +0900

    feat: 수강신청 로그인 기능

---
+++ login.js
@@ -0,0 +1,8 @@
+const loginButton = document.getElementById('login-button');
+if (loginButton) {
+  loginButton.addEventListener('click', () => {
+    window.location.href = 'game.html';
+  });
+}
+
+
